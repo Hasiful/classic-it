702fae16c147ef0497043033d8491156b3ffb3a3
hasiful <hasifulislam32@gmail.com>
2023-03-24 12:42:33 +0600

    classic it

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+var header = document.getElementsByClassName("header_nav")[0];
+var sticky = header.offsetTop;
+
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+})
+
+$('.video_popup').magnificPopup({
+    type: 'iframe'
+});
